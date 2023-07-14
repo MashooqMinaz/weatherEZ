9e7fd93fc3103bdf067a36d7e98f2e0723323c14
MashooqMinaz <mashooqminaz@gmail.com>
2023-07-14 11:14:31 +0530

    weather app v-2

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     extend: {
       colors: {
         'custom-grey' : '#b5becc',
+        'custom-light-blue': "#77b4d6",
+        'custom-medium-blue': "#6387bb",
+        'custom-dark-blue': "#5360ab",
       }
     },
   },
